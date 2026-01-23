37b1479d6477e6b38c34c3c53db56aa232e35808
Lucas <lucaspereiradelima2020@gmail.com>
2026-01-23 19:34:38 -0300

    Criando os atributos da classe Endereco.

--- principal/src/com/github/lucasdevrj/modelos/Endereco.java
+++ principal/src/com/github/lucasdevrj/modelos/Endereco.java
@@ -1,4 +1,18 @@
 package com.github.lucasdevrj.modelos;
 
 public class Endereco {
+
+    private String cep;
+    private String logradouro;
+    private String comeplemento;
+    private String unidade;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String estado;
+    private String regiao;
+    private String ibge;
+    private String guia;
+    private String ddd;
+    private String siafi;
 }
