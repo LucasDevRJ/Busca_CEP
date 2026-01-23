4e32659d1016c78b452e0a83c00bad5b6917a5c9
Lucas <lucaspereiradelima2020@gmail.com>
2026-01-23 20:13:43 -0300

    Método para pesquisar CEP.

--- principal/src/com/github/lucasdevrj/modelos/ViaCepAPI.java
+++ principal/src/com/github/lucasdevrj/modelos/ViaCepAPI.java
@@ -1,17 +1,20 @@
 package com.github.lucasdevrj.modelos;
 
+import com.github.lucasdevrj.excecoes.ErroCepInvalidoException;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class ViaCepAPI {
 
     private String cep;
     private final String api = "viacep.com.br/ws"; // 22710270/json
     private final String formato = "json";
-    private final String url = "%s/%s/%s".formatted(api, cep, formato);
+    private String url = "%s/%s/%s".formatted(api, cep, formato);
 
     private HttpClient criaInstanciaDoClienteHttp() {
         HttpClient cliente = HttpClient.newHttpClient();
@@ -29,5 +32,20 @@ public class ViaCepAPI {
         HttpResponse<String> resposta = cliente.send(requisicao, HttpResponse.BodyHandlers.ofString());
         String corpoDaResposta = resposta.body();
         return corpoDaResposta;
+    }
+
+    private void pesquisaCep() {
+        Scanner entradaDeDados = new Scanner(System.in);
+
+        System.out.print("Digite o CEP desejado: ");
+        String cepDigitado = entradaDeDados.nextLine();
+
+        if (cepDigitado.length() != 8) {
+            throw new ErroCepInvalidoException("CEP inválido! Digite somente 8 números. Exemplo: \"01001000\"");
+        } else if (cepDigitado.matches(".*\\D.*")) {
+            throw new ErroCepInvalidoException("CEP inválido! Digite somente números. Exemplo: \"01001000\"");
+        }
+
+        cep = cepDigitado;
     }
 }
