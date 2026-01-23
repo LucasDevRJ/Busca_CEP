e24574225e0b96b869f78c97d47f522049429710
Lucas <lucaspereiradelima2020@gmail.com>
2026-01-23 19:49:47 -0300

    Método de pegar requisição e atributos da classe.

--- principal/src/com/github/lucasdevrj/modelos/ViaCepAPI.java
+++ principal/src/com/github/lucasdevrj/modelos/ViaCepAPI.java
@@ -1,11 +1,23 @@
 package com.github.lucasdevrj.modelos;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
 public class ViaCepAPI {
+
+    private String cep;
+    private final String api = "viacep.com.br/ws"; // 22710270/json
+    private final String formato = "json";
+    private final String url = "%s/%s/%s".formatted(api, cep, formato);
 
     private HttpClient criaInstanciaDoClienteHttp() {
         HttpClient cliente = HttpClient.newHttpClient();
         return cliente;
+    }
+
+    private HttpRequest criaRequisicaoHttp() {
+        HttpRequest requisicao = HttpRequest.newBuilder().uri(URI.create(url)).build();
+        return requisicao;
     }
 }
