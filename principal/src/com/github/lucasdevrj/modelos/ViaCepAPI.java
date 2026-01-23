c86e081bad5826078ce05da8889d2ebb8023b5cf
Lucas <lucaspereiradelima2020@gmail.com>
2026-01-23 19:42:30 -0300

    Método que retorna o cliente HTTP.

--- principal/src/com/github/lucasdevrj/modelos/ViaCepAPI.java
+++ principal/src/com/github/lucasdevrj/modelos/ViaCepAPI.java
@@ -1,4 +1,11 @@
 package com.github.lucasdevrj.modelos;
 
+import java.net.http.HttpClient;
+
 public class ViaCepAPI {
+
+    private HttpClient criaInstanciaDoClienteHttp() {
+        HttpClient cliente = HttpClient.newHttpClient();
+        return cliente;
+    }
 }
